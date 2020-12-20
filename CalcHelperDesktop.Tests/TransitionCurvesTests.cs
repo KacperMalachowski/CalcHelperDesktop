4e370790813fbdf93c44802fe4de3f0d1169ade6
Kacper Malachowski <kacpermaachowski@gmail.com>
2020-12-20 19:57:31 +0100

    Add calculations for trransition curves

---
+++ CalcHelperDesktop.Tests/TransitionCurvesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CalcHelperDesktop.Calcs;
+
+namespace CalcHelperDesktop.Tests
+{
+    [TestFixture]
+    public class TransitionCurvesTests
+    {
+        [TestCase(90, 975, 0.35)]
+        [TestCase(23.564, 904.53, 0.03)]
+        public void TestTransitionCurveBetweenStraightAndCurve(double Length, double Radius, double Expected)
+        {
+            double Result = TransitionCurves.StraightArc(Length, Radius);
+
+            Assert.AreEqual(Expected, Result);
+        }
+    }
+}
---
+++ CalcHelperDesktop/Calcs/TransitionCurves.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcHelperDesktop.Calcs
+{
+    public static class TransitionCurves
+    {
+        public static double StraightArc(double Length, double Radius)
+        {
+            double asin = Math.Asin(Length / (2 * Radius));
+            double kappa = 1 / Math.Cos(asin);
+            double result = (kappa * Math.Pow(Length, 2) / (6 * Radius)) - (Radius * (1 - Math.Cos(asin)));
+            return Math.Round(result, 2);
+        }
+    }
+}
