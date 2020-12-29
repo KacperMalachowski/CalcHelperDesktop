7b16ba76496f0bb6c3e5860e9e568c2620cdf641
Kacper Malachowski <kacpermaachowski@gmail.com>
2020-12-29 10:50:22 +0100

    Add Calcs for shift Arc To Arc

--- CalcHelperDesktop.Tests/TransitionCurvesTests.cs
+++ CalcHelperDesktop.Tests/TransitionCurvesTests.cs
@@ -11,9 +11,19 @@ namespace CalcHelperDesktop.Tests
     {
         [TestCase(90, 975, 0.35)]
         [TestCase(23.564, 904.53, 0.03)]
-        public void TestTransitionCurveBetweenStraightAndCurve(double Length, double Radius, double Expected)
+        [TestCase(80, 450, 0.60)]
+        public void TestShiftForTransitionCurveBetweenStraightAndArc(double Length, double Radius, double Expected)
         {
-            double Result = TransitionCurves.StraightArc(Length, Radius);
+            double Result = TransitionCurves.ShiftForStraightToArc(Length, Radius);
+
+            Assert.AreEqual(Expected, Result);
+        }
+
+        [TestCase(30, 800, 400, 0.05)]
+        [TestCase(30, 250, 410, -0.06)]
+        public void TestShiftForTransitionCurveBetweenTwoArcs(double Length, double FirstRadius, double SecondRadius, double Expected)
+        {
+            double Result = TransitionCurves.ShiftForArcToArc(Length, FirstRadius, SecondRadius);
 
             Assert.AreEqual(Expected, Result);
         }
--- CalcHelperDesktop/Calcs/TransitionCurves.cs
+++ CalcHelperDesktop/Calcs/TransitionCurves.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace CalcHelperDesktop.Calcs
 {
-    public static class TransitionCurves
+    public class TransitionCurves
     {
-        public static double StraightArc(double Length, double Radius)
+        public static double ShiftForStraightToArc(double Length, double Radius)
         {
             double asin = Math.Asin(Length / (2 * Radius));
             double kappa = 1 / Math.Cos(asin);
             double result = (kappa * Math.Pow(Length, 2) / (6 * Radius)) - (Radius * (1 - Math.Cos(asin)));
+            return Math.Round(result, 2);
+        }
+
+        public static double ShiftForArcToArc(double Length, double FirstRadius, double SecondRadius)
+        {
+            double result = (Math.Pow(Length, 2) / (6 * SecondRadius) - SecondRadius * (1 - Math.Cos(Length / (2 * SecondRadius))))
+                - ((Math.Pow(Length, 2) / (6 * FirstRadius)) - (FirstRadius * (1 - Math.Cos(Length / (2 * FirstRadius)))));
+
             return Math.Round(result, 2);
         }
     }
